a2d022cba2256593ec93e6f854a1240d91f782b2
Vickorock <vickobernard35@gmail.com>
2020-03-26 15:24:29 +0100

    test pour corentin

--- src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -261,6 +261,8 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 			newUtilisateur.setPseudo(rs.getString("pseudo"));
 
 			return newUtilisateur;
+			
+			//commentaire
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
