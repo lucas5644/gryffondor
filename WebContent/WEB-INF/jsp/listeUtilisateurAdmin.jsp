10824df6e25e197c3734df84f693a8267a7184ff
corentin <guillois.co@gmail.com>
2020-04-01 09:35:43 +0200

    corentin

---
+++ WebContent/WEB-INF/jsp/listeUtilisateurAdmin.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Liste Utilisateur</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand"
+				href="<%=request.getContextPath()%>/AccueilUtilisateur">Mon
+				application d'enchères</a>
+
+			<div>
+				<a class="btn btn-primary nav-link" role="button"
+					href="AjouterArticle">Vendre un Article</a>
+			</div>
+			<div>
+				<a class="btn btn-primary nav-link" role="button"
+					href="#">Liste Utilisateur</a>
+			</div>
+			<div>
+				<a class="btn btn-primary nav-link" role="button" href="monprofil">Mon
+					profil</a>
+			</div>
+			<div>
+				<a class="btn btn-primary nav-link" role="button" href="deconnexion">Déconnexion</a>
+			</div>
+		</div>
+	</nav>
+
+	<div class="col-lg-12 col-md-12 col-sm-12 portfolio-item">
+		<div class="card h-100">
+			<div class="card-body contenu">
+				<div class="contenu">
+					<c:forEach var="utilisateur" items="${ListeUtilisateur}">
+						<form action="#" method="post">
+							<div class="col-lg-12 d-inline-block contenu portfolio-item">
+									
+								 <a>no utilisateur : ${utilisateur.noUtilisateur}</a><br>
+								 <a>Pseudo : ${utilisateur.pseudo}</a><br>
+								 <a>Nom : ${utilisateur.nom}</a><br> 
+								 <a>Prénom	: ${utilisateur.prenom}</a>
+								 <a>Email	: ${utilisateur.email}</a>
+								 <a>Telephone	: ${utilisateur.telephone}</a>
+								 <a>Rue	: ${utilisateur.rue}</a>
+								 <a>Code Postal	: ${utilisateur.codePostal}</a>
+								 <a>Ville	: ${utilisateur.ville}</a>
+								 <a>Mot de passe : ${utilisateur.motDePasse}</a>
+								 <a>Crédit	: ${utilisateur.credit}</a>
+								 <a>Administrareur	: ${utilisateur.administrateur}</a>
+							<input type="Button" value="" name="Supprimer utilisateur">
+							<input type="Button" value="" name="desactiver un comptes">
+							
+							</div>
+						</form>
+					</c:forEach>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
---
+++ src/fr/eni/encheres/servlets/ServletDeleteUserAdmin.java
@@ -0,0 +1,71 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.EncheresManager;
+import fr.eni.encheres.exception.BusinessException;
+
+/**
+ * Servlet implementation class ServletDeleteUserAdmin
+ */
+@WebServlet("/DeleteUserAdmin")
+public class ServletDeleteUserAdmin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Integer> listeCodesErreur = new ArrayList<Integer>();
+		request.setAttribute("listeCodesErreur",listeCodesErreur);
+		
+		String noUtilisateur = request.getParameter("noUtilisateur");
+		EncheresManager enchereManager = new EncheresManager();
+		
+		int noUtilisaateur = Integer.parseInt(noUtilisateur);
+		boolean verification;
+		try {
+			
+			verification = enchereManager.removeUtilisateur(noUtilisaateur);
+			
+			if(verification == true) {
+			
+				RequestDispatcher rd = request.getRequestDispatcher("/accueil");
+				
+				rd.forward(request, response);		
+			}
+			else {
+				request.setAttribute("echec", "Erreur, le compte n'a pas été suprimé");
+				RequestDispatcher rd = request.getRequestDispatcher("/");
+				rd.forward(request, response);
+			}
+			
+		} catch (BusinessException e) {
+			request.setAttribute("echec", "Erreur, le compte n'a pas été suprimé");
+			RequestDispatcher rd = request.getRequestDispatcher("/accueil");
+			rd.forward(request, response);			
+			e.printStackTrace();
+		}
+		
+	}
+
+}
