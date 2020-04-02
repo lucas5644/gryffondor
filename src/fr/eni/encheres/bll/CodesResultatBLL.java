c3e9bab4c4e5996848572a5378765170c2b6cfa9
corentin <guillois.co@gmail.com>
2020-04-02 10:36:06 +0200

    corentin

--- src/fr/eni/encheres/bll/CodesResultatBLL.java
+++ src/fr/eni/encheres/bll/CodesResultatBLL.java
@@ -9,6 +9,10 @@ public abstract class CodesResultatBLL {
 	public static final int ENCHERE_ERREUR=20001;
 	public static final int ENCHERE_ERREUR_2 = 20002;
 	public static final int DATE_ERREUR_2=20003;
+	
+	/**
+	 * Erreur du check user
+	 */
 	public static final int FORMAT_CODE_POSTAL_ERREUR=20004;
 	public static final int FORMAT_VILLE_ERREUR=20005;
 	public static final int FORMAT_NOM_ERREUR=20006;
