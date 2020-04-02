b122c7fc9894ea90a1cbdbcd743e04b81894612f
corentin <guillois.co@gmail.com>
2020-04-02 17:39:51 +0200

    corentin

---
+++ src/fr/eni/encheres/servlets/ServletEmail.java
@@ -0,0 +1,42 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ServletEmail
+ */
+@WebServlet("/Email")
+public class ServletEmail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	Utilisateur user = new Utilisateur();
+	String email= request.getParameter("email");
+	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/NewFile.jsp");
+	rd.forward(request, response);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/fr/eni/encheres/servlets/ServletReinitialisationMDP.java
@@ -0,0 +1,65 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bll.EncheresManager;
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.exception.BusinessException;
+
+/**
+ * Servlet implementation class ServletReinitialisationMDP
+ */
+@WebServlet("/ReinitialisationMDP")
+public class ServletReinitialisationMDP extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Utilisateur user = new Utilisateur();
+		Utilisateur user1;
+		
+		 String email=request.getParameter("email");
+	   
+		   
+		 String nouveauMotDePasse=request.getParameter("nouveauMotDePasse");
+		 String cNouveauMotDePasse=request.getParameter("cNouveauMotDePasse");
+		 if(nouveauMotDePasse.equals(cNouveauMotDePasse)){
+			 user.setMotDePasse(cNouveauMotDePasse);
+		 }
+		
+		 EncheresManager enchereManager = new EncheresManager();
+
+			try {
+				//user1 = enchereManager.updateMDP(user);
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
+
+				rd.forward(request, response);
+
+			} catch (BusinessException e) {
+				e.printStackTrace();
+				request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp");
+				rd.forward(request, response);
+			}
+		}
+		
+		
+
+}
