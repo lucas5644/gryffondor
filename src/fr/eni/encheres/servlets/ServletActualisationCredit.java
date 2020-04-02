f59cc5df812a4e47875b4823ac9c990894812132
Lucas <lucas.brient2020@campus-eni.fr>
2020-04-02 20:53:56 +0200

    Servlet actualisation Credit

---
+++ src/fr/eni/encheres/servlets/ServletActualisationCredit.java
@@ -0,0 +1,37 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ServletActualisationCredit
+ */
+@WebServlet("/ServletActualisationCredit")
+public class ServletActualisationCredit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Utilisateur userCourant = new Utilisateur();
+		userCourant = (Utilisateur) session.getAttribute("userConnected");
+		System.out.println(userCourant);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueilConnecte.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
